cc3af9709ca9f5a610b117ffc718082f3b42d13d
park hyunbin <wise7231@gmail.com>
2022-10-07 20:18:35 +0900

    default setting

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  app.enableCors();
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
