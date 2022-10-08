22036cc926585e538c09d6495064303aafdd19a1
park hyunbin <wise7231@gmail.com>
2022-10-08 15:50:46 +0900

    rds connection 정보 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'jj-database.cyix6flvbh8e.ap-northeast-2.rds.amazonaws.com',
+      port: 3306,
+      username: 'admin',
+      password: 'jjjj1234',
+      database: 'jj-dev-db',
+      entities: ['dist/src/**/*.entity{.ts,.js}'],
+      synchronize: false,
+      bigNumberStrings: false,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
