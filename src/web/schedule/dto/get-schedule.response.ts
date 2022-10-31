6f4aed9652517bebd22f8ff9f5464382e503e929
park hyunbin <wise7231@gmail.com>
2022-10-31 23:27:31 +0900

    일정 상세 조회 api 수정 (response isArray 추가)

--- src/web/schedule/dto/get-schedule.response.ts
+++ src/web/schedule/dto/get-schedule.response.ts
@@ -146,6 +146,7 @@ class Schedule {
   @ApiProperty({
     example: ['임산부, 유아, 어린이', '교통약자 동반'],
     description: '출발 공항 서비스',
+    isArray: true,
   })
   @IsArray()
   departureAirportService: Array<String>;
@@ -153,6 +154,7 @@ class Schedule {
   @ApiProperty({
     example: ['임산부, 유아, 어린이', '교통약자 동반'],
     description: '도착 공항 서비스',
+    isArray: true,
   })
   @IsArray()
   arrivalAirportService: Array<String>;
@@ -160,6 +162,7 @@ class Schedule {
   @ApiProperty({
     example: ['임산부, 유아, 어린이', '교통약자 동반'],
     description: '항공사 서비스',
+    isArray: true,
   })
   @IsArray()
   airlineService: Array<String>;
